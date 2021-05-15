b092c4fea914d089dd2ca276a9adb51ce4d59c87
Cora Phoenix <christopher0brandao42@gmail.com>
2021-05-15 17:28:14 -0300

    Changes on file

--- frontend/static/js/views/Dashboard.js
+++ frontend/static/js/views/Dashboard.js
@@ -8,9 +8,9 @@ export default class extends AbstractView {
 
     async getHtml() {
         return `
-            <h1>Welcome back, Dom</h1>
+            <h1>Welcome to Curriculum Generator Project</h1>
             <p>
-                Fugiat voluptate et nisi Lorem cillum anim sit do eiusmod occaecat irure do. Reprehenderit anim fugiat sint exercitation consequat. Sit anim laborum sit amet Lorem adipisicing ullamco duis. Anim in do magna ea pariatur et.
+                This is a place where you can automatically generate a curriculum by only inserting your data.
             </p>
             <p>
                 <a href="/posts" data-link>View recent posts</a>.
